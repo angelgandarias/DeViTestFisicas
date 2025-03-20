e291503432c2f939f973324518cd84f64f36684f
angelgandarias <angel.gandarias@opendeusto.es>
2025-03-20 09:06:01 +0100

    Script CubeDeleter

--- Assets/CapsuleController.cs
+++ Assets/CapsuleController.cs
@@ -34,12 +34,4 @@ public class CapsuleController : MonoBehaviour
 
         }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if ( collision.gameObject.name != "Plane")
-        {
-            Destroy(collision.gameObject);
-        }
-        
-    }
 }
